fe94bb94beb10496c1ffad5cc0a27452326e0f45
Paulo Lieuthier <paulolieuthier@gmail.com>
2015-05-27 12:15:01 -0300

    Procedure de gerar relatório do estoque

--- PROCEDURES.sql
+++ PROCEDURES.sql
@@ -3,10 +3,18 @@ DELIMITER $$
 USE projetobd$$
 
 DROP PROCEDURE IF EXISTS gerar_relatorio_funcionario$$
+DROP PROCEDURE IF EXISTS gerar_relatorio_estoque$$
 
 CREATE PROCEDURE gerar_relatorio_funcionario (campo varchar(10), ordenacao varchar(4))
 BEGIN
     SET @query = CONCAT('SELECT cpf, nome, salario, funcao FROM funcionario ORDER BY ', campo, ' ', ordenacao);
+    PREPARE stmt FROM @query;
+    EXECUTE stmt;
+END$$
+
+CREATE PROCEDURE gerar_relatorio_estoque (campo varchar(10), ordenacao varchar(4))
+BEGIN
+    SET @query = CONCAT('SELECT id, nome, quantidade, preco, descricao FROM produto_estoque ORDER BY ', campo, ' ', ordenacao);
     PREPARE stmt FROM @query;
     EXECUTE stmt;
 END$$
