6b2c34a85ebbaf2b9e05c8618540e54ec37bb345
Paulo Lieuthier <paulolieuthier@gmail.com>
2015-05-28 09:02:24 -0300

    Procedure de gerar relatório da movimentação

--- SQL/PROCEDURES.sql
+++ SQL/PROCEDURES.sql
@@ -4,6 +4,7 @@ USE projetobd$$
 
 DROP PROCEDURE IF EXISTS gerar_relatorio_funcionario$$
 DROP PROCEDURE IF EXISTS gerar_relatorio_estoque$$
+DROP PROCEDURE IF EXISTS gerar_relatorio_movimentacao$$
 
 CREATE PROCEDURE gerar_relatorio_funcionario (campo varchar(10), ordenacao varchar(4))
 BEGIN
@@ -15,6 +16,13 @@ END$$
 CREATE PROCEDURE gerar_relatorio_estoque (campo varchar(10), ordenacao varchar(4))
 BEGIN
     SET @query = CONCAT('SELECT id, nome, quantidade, preco, descricao FROM produto_estoque ORDER BY ', campo, ' ', ordenacao);
+    PREPARE stmt FROM @query;
+    EXECUTE stmt;
+END$$
+
+CREATE PROCEDURE gerar_relatorio_movimentacao (campo varchar(10), ordenacao varchar(4), data_inicio date, data_final date)
+BEGIN
+    SET @query = CONCAT('SELECT id, nome, quantidade, preco, descricao FROM produto_estoque WHERE data_movimentacao BETWEEN ', data_inicial, ' AND ', data_final, ' ORDER BY ', campo, ' ', ordenacao);
     PREPARE stmt FROM @query;
     EXECUTE stmt;
 END$$
