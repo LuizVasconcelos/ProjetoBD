bf9b3853e46d5be2b0a59564168563d051d7ea6e
Paulo Lieuthier <paulolieuthier@gmail.com>
2015-06-01 10:49:37 -0300

    Fix trigger: strcomp -> strcmp

--- SQL/TRIGGERS.sql
+++ SQL/TRIGGERS.sql
@@ -12,9 +12,9 @@ BEGIN
 	DECLARE funcao VARCHAR(20);
 	-- se for uma compra
 	IF NEW.tipo = 'c' THEN
-		SET funcao := (SELECT FN.nome FROM funcao FN JOIN funcionario F ON F.funcao = FN.nome);
+		SET funcao := (SELECT FN.nome FROM funcao FN JOIN funcionario F ON F.funcao = FN.nome WHERE F.cpf = NEW.cpf);
         -- se o funcionário não for gerente
-        IF strcomp(funcao, 'gerente') <> 0 THEN
+        IF STRCMP(funcao, 'gerente') <> 0 THEN
 			SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Funcionário não possui autorização para realizar compra.';
         END IF;
     END IF;
