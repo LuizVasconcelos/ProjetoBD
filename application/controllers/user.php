6269ced4f8bac3c3a20a6fdc23ebd55ffa23feae
Paulo Lieuthier <paulolieuthier@gmail.com>
2015-06-05 19:57:35 -0300

    Login corrigido para não-gerentes

--- application/controllers/user.php
+++ application/controllers/user.php
@@ -2,11 +2,6 @@
 
 class User extends CI_Controller {
 
-    /**
-    * Check if the user is logged in, if he's not, 
-    * send him to the login page
-    * @return void
-    */	
 	function index()
 	{
         if ($this->session->userdata('is_logged_in'))
@@ -14,24 +9,16 @@ class User extends CI_Controller {
             if ($this->session->userdata('is_manager'))
                 redirect('employees');
             else
-                redirect('sell');
+                redirect('products');
         }
         else
         	$this->load->view('login');	
 	}
 
-    /**
-    * encript the password 
-    * @return mixed
-    */	
     function __encrip_password($password) {
         return md5($password);
     }	
 
-    /**
-    * check the username and the password with the database
-    * @return void
-    */
 	function validate_credentials()
 	{	
 
@@ -43,7 +30,7 @@ class User extends CI_Controller {
 
 		$is_valid = $this->UsersModel->validate($user_name, $password);
 		
-		if($is_valid)
+		if ($is_valid)
 		{
             $var = $this->UsersModel->check_manager($user_name);
 			$data = array(
@@ -56,7 +43,7 @@ class User extends CI_Controller {
             if ($var)
                 redirect('employees');
             else
-                redirect('sell');
+                redirect('products');
 		}
 		else // incorrect username or password
 		{
@@ -65,58 +52,6 @@ class User extends CI_Controller {
 		}
 	}	
 
-    /**
-    * The method just loads the signup view
-    * @return void
-    */
-	function signup()
-	{
-		$this->load->view('signup_form');	
-	}
-	
-
-    /**
-    * Create new user and store it in the database
-    * @return void
-    */	
-	function create_member()
-	{
-		$this->load->library('form_validation');
-		
-		// field name, error message, validation rules
-		$this->form_validation->set_rules('first_name', 'Name', 'trim|required');
-		$this->form_validation->set_rules('last_name', 'Last Name', 'trim|required');
-		$this->form_validation->set_rules('email_address', 'Email Address', 'trim|required|valid_email');
-		$this->form_validation->set_rules('username', 'Username', 'trim|required|min_length[4]');
-		$this->form_validation->set_rules('password', 'Password', 'trim|required|min_length[4]|max_length[32]');
-		$this->form_validation->set_rules('password2', 'Password Confirmation', 'trim|required|matches[password]');
-		$this->form_validation->set_error_delimiters('<div class="alert alert-error"><a class="close" data-dismiss="alert">×</a><strong>', '</strong></div>');
-		
-		if($this->form_validation->run() == FALSE)
-		{
-			$this->load->view('signup_form');
-		}
-		
-		else
-		{			
-			$this->load->model('UsersModel');
-			
-			if($query = $this->UsersModel->create_member())
-			{
-				$this->load->view('signup_successful');			
-			}
-			else
-			{
-				$this->load->view('signup_form');			
-			}
-		}
-		
-	}
-	
-	/**
-    * Destroy the session, and logout the user.
-    * @return void
-    */		
 	function logout()
 	{
 		$this->session->sess_destroy();
