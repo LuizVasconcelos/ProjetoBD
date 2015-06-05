8042a865433d9b76a49efcecf7af689b1c763b97
Paulo Lieuthier <paulolieuthier@gmail.com>
2015-06-05 20:03:19 -0300

    SQL necessário para sessão e cookies do site

---
+++ SQL/CODEIGNITER.sql
@@ -0,0 +1,27 @@
+SET foreign_key_checks = 0;
+DROP TABLE IF EXISTS ci_cookies CASCADE;
+DROP TABLE IF EXISTS ci_sessions CASCADE;
+SET foreign_key_checks = 1;
+
+CREATE TABLE `ci_cookies` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `cookie_id` varchar(255) DEFAULT NULL,
+  `netid` varchar(255) DEFAULT NULL,
+  `ip_address` varchar(255) DEFAULT NULL,
+  `user_agent` varchar(255) DEFAULT NULL,
+  `orig_page_requested` varchar(120) DEFAULT NULL,
+  `php_session_id` varchar(40) DEFAULT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+CREATE TABLE `ci_sessions` (
+  `session_id` varchar(40) NOT NULL DEFAULT '0',
+  `ip_address` varchar(45) NOT NULL DEFAULT '0',
+  `user_agent` varchar(120) NOT NULL,
+  `last_activity` int(10) unsigned NOT NULL DEFAULT '0',
+  `user_data` text NOT NULL,
+  PRIMARY KEY (`session_id`),
+  KEY `last_activity_idx` (`last_activity`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
